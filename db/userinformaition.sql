e73aa78777ff5b1f07852fb9768eb871eb467e33
jw0302 <wkdwlsdnjs41@naver.com>
2023-01-26 17:43:23 +0900

    SQL 저장

--- db/userinformaition.sql
+++ db/userinformaition.sql
@@ -15,6 +15,28 @@ FROM
 	`user_mst` `um`
     LEFT OUTER JOIN `role_dtl` `rd` ON(`rd`.`user_id` = `um`.`user_id`)
     LEFT OUTER JOIN `role_mst` `rm` ON(`rm`.`role_id` = `rd`.`role_dtl_id`);
+    
+SELECT 
+	um.user_id,
+    um.username,
+    um.password,
+    um.name,
+    um.email,
+    rd.role_dtl_id,
+    rd.role_id,
+    rd.user_id,
+    rm.role_id,
+    rm.role_name
+FROM
+	user_mst um
+    LEFT OUTER JOIN role_dtl rd ON(rd.user_id = um.user_id)
+    LEFT OUTER JOIN role_mst rm ON(rm.role_id = rd.role_id);
+    
+SELECT
+	*
+FROM
+	`user_mst` `um`
+    LEFT OUTER JOIN `role_dtl` `rd` ON(`rd`.`user_id` = `um`.`user_id`);
 	
     
     
